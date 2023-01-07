f39c8df8f7e0de6f309e89071e2d7ca87e651a39
Anthony ZORZETTO <zorzetto.anthony@gmail.com>
2023-01-07 16:32:49 +0100

    1st commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.azodev.firstApp',
+  appName: 'First App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
