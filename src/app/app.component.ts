d0e9fb958e109cd52e1abd59b182998457589576
Anthony ZORZETTO <zorzetto.anthony@gmail.com>
2023-01-12 00:20:55 +0100

    Add back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { App } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  /*constructor(
+    private platform: Platform,
+    private router: Router
+  ) {
+      this.platform.backButton.subscribeWithPriority(-1, () => {
+          const url = this.router.url;
+          console.log(url);
+          if (url != 'tabs/tab1') {
+              this.router.navigate(['tabs/tab1']);
+          } else if (url == 'tabs/tab1') {
+              App.exitApp();
+              console.log('exit');
+          }
+      });
+  }*/
+  constructor(private platform: Platform, private router: Router) {
+    this.platform.backButton.subscribeWithPriority(5, () => {
+      console.log('Another handler was called!');
+      const url = this.router.url;
+      console.log(url);
+      if (url != "tabs/tab1") {
+          this.router.navigate(['tabs/tab1']);
+      } 
+      else if (url == "tabs/tab1") {
+          App.exitApp();
+          console.log('exit');
+      }
+    });
+  
+    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+      console.log('Handler was called!');
+  
+      processNextHandler();
+    });
+  }
 }
